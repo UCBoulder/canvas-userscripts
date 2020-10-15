045068dd6e4213091451b7b8dfbc43b2907b9a8c
James Ashby <James.Ashby@Colorado.EDU>
2020-10-15 11:12:39 -0600

    Revert changes to Zoom attendance script

--- zoom_attendance.user.js
+++ zoom_attendance.user.js
@@ -7,7 +7,7 @@
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js
 // @run-at       document-idle
-// @version      1.2.0
+// @version      1.3.0
 // ==/UserScript==
 
 /* globals $ Papa */
@@ -56,12 +56,12 @@ function getAllPages(url, callback) {
 function getRemainingPages(nextUrl, listSoFar, callback) {
     $.getJSON(nextUrl, function(responseList, textStatus, jqXHR) {
         let nextLink = null;
-        for (const linkEntry of jqXHR.getResponseHeader("link").split(',')) {
+        $.each(jqXHR.getResponseHeader("link").split(','), function (linkIndex, linkEntry) {
             if (linkEntry.split(';')[1].includes('rel="next"')) {
                 nextLink = linkEntry.split(';')[0].slice(1, -1);
             }
-        }
-        if (nextLink === null) {
+        });
+        if (nextLink == null) {
             // all pages have been retrieved
             callback(listSoFar.concat(responseList));
         } else {
@@ -88,10 +88,9 @@ function openOptionsForm(assignName, submitAction) {
 <p>
 Import a Zoom Participants spreadsheet as attendance grades to a new column (assignment).
 </p>
-<a href="https://oit.colorado.edu/tutorial/import-zoom-attendance-canvas-gradebook" target="_blank">Help Guide</a>
 <hr>
 <form id="zoom_form">
-<label>Assignment name</label>
+<label>New assignment name</label>
 <input autocomplete="off" name="zoom_assign_name" value="${assignName}">
 <br>
 <br>
@@ -159,14 +158,10 @@ function popUpIf(condition, text, callback) {
     }
 }
 
-// Open the Confirm Overwrite dialog
-function confirmOverwrite(text, callback) {
-    $("#zoom_confirm").html(`<p>${text}</p>`)
-        .dialog({ buttons: {
-            Overwrite: function() { $(this).dialog("close"); callback(); },
-            Cancel: function() { $(this).dialog("close"); }
-        } });
-    $("#zoom_confirm").dialog("open");
+// Turn an Excel-style serialDate into a date string
+function dateFromSerial(serialDate) {
+    const date = new Date(Date.UTC(0, 0, serialDate, 0));
+    return `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`
 }
 
 // Get the data out of the selected spreadsheet file
@@ -214,20 +209,13 @@ function parseImport(importJson, callback) {
     // Extract users from the file
     let importUsers = [];
     let noImports = [];
-    let uniqueUsers = new Set();
     for (const row of importJson.slice(headerRow + 1)) {
         if (row.length >= 1 && row[1] && row[1].includes('@')) {
             const minutes = row.length >= 2 ? row[minutesCol] : 0;
             importUsers.push({'username': row[1].split("@")[0], 'matched': false, 'minutes': minutes});
-            uniqueUsers.add(row[1]);
         } else if (row.length > 0 && row[0]) {
             noImports.push(row[0]);
         }
-    }
-    if (uniqueUsers.size < importUsers.length) {
-        popUp(`ERROR - The spreadsheet contains ${importUsers.length - uniqueUsers.size} duplicate users. Be sure to select "Show unique users" when exporting the Zoom report and try again.`, null);
-        $("#zoom_file").show();
-        return;
     }
     const customText = noImports.length > 10 ? "The first ten of these u" : "U";
     popUpIf(noImports.length > 0, `NOTICE - ${noImports.length} users will not be imported because their email was not captured by Zoom. ${customText}sers are listed below.<br><br>${noImports.slice(0, 10).join('<br>')}`, function() {
@@ -263,135 +251,102 @@ function parseImport(importJson, callback) {
     });
 }
 
-// Find an existing assignment or create a new one and send the Canvas assignment object and list of existing submissions to callback
-function identifyAssignment(formData, callback) {
+// Actually save the grades to Canvas based on data from the spreadsheet and the dialog form
+function saveAttendance(formData, attendData) {
     const assignName = getFormValue('zoom_assign_name', formData);
     const assignPoints = getFormValue('zoom_points', formData);
+    const minMinutes = getFormValue('zoom_min_minutes', formData);
     const assignGroup = getFormValue('zoom_assign_group', formData);
     const courseId = window.location.href.split('/')[4];
 
-    getAllPages(`/api/v1/courses/${courseId}/assignment_groups/${assignGroup}/assignments`, function(assignments) {
-        let match = assignments.find(item => item.name === assignName);
-        function overwrite() {
-            popUpIf(match.points_possible != assignPoints,
-                    `NOTE - When overwriting an assignment, the total assignment points will not be adjusted: it will remain at ${match.points_possible}.`,
-                    function() {
-                // Pull the assignment's existing submissions
-                getAllPages(`/api/v1/courses/${courseId}/assignments/${match.id}/submissions`, function(submissions) {
-                    callback(match, submissions);
-                });
-            });
-        }
-        function createNew() {
-            $.ajax({
-                url: `/api/v1/courses/${courseId}/assignments`,
-                type: 'POST',
-                data: { 'assignment[name]': assignName,
-                       'assignment[submission_types][]': 'none',
-                       'assignment[points_possible]': assignPoints,
-                       'assignment[assignment_group_id]': assignGroup,
-                       'assignment[published]': true },
-                dataType: "text"
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                popUp(`ERROR ${jqXHR.status} while creating assignment. Please refresh and try again.`, null);
-                $("#zoom_file").show();
-            }).done(function (responseText) {
-                callback(JSON.parse(responseText), []);
-            });
-        }
-
-        if (match) {
-            confirmOverwrite(`The assignment "${assignName}" already exists. Do you wish to overwrite? If so, students present in the current attendance file will be added to those already scored as present in the assignment.`,
-                             overwrite);
-        } else {
-            createNew();
-        }
-    });
-}
-
-// Actually save the grades to Canvas based on data from the spreadsheet, the dialog form, and the identified assignment object (plus an optional list of existing submissions)
-function saveAttendance(formData, attendData, assignment, submissions) {
-    const minMinutes = getFormValue('zoom_min_minutes', formData);
-    const courseId = window.location.href.split('/')[4];
-
-    // "mute" assignment (i.e. set the post policy to "manual" so students won't see the new grades by default)
+    // create assignment
     $.ajax({
-        url: `/api/graphql`,
+        url: `/api/v1/courses/${courseId}/assignments`,
         type: 'POST',
-        data: String.raw`{"operationName":"SetAssignmentPostPolicy","variables":{"assignmentId":"${assignment.id}","postManually":true},"query":"mutation SetAssignmentPostPolicy($assignmentId: ID!, $postManually: Boolean!) {\n  setAssignmentPostPolicy(input: {assignmentId: $assignmentId, postManually: $postManually}) {\n    postPolicy {\n      postManually\n      __typename\n    }\n    errors {\n      attribute\n      message\n      __typename\n    }\n    __typename\n  }\n}\n"}`,
-        contentType: "application/json",
+        data: { 'assignment[name]': assignName,
+               'assignment[submission_types][]': 'none',
+               'assignment[points_possible]': assignPoints,
+               'assignment[assignment_group_id]': assignGroup,
+               'assignment[published]': true },
         dataType: "text"
     }).fail(function (jqXHR, textStatus, errorThrown) {
-        popUp(`ERROR ${jqXHR.status} while muting assignment. Please delete the empty assignment before trying again.`, null);
+        popUp(`ERROR ${jqXHR.status} while creating assignment. Please refresh and try again.`, null);
         $("#zoom_file").show();
     }).done(function (responseText) {
-        let userGrades = []
-        // Only plan to send the score if there isn't already a positive score for that user
-        let nonZeroSubs = submissions.filter(item => item.score > 0);
-        for (const user of attendData.users) {
-            let match = nonZeroSubs.find(item => item.user_id == user.userId);
-            if (!match) {
-                userGrades.push({ user: user.userId, username: user.username, grade: user.minutes >= minMinutes ? assignment.points_possible : 0 });
+
+        // "mute" assignment (i.e. set the post policy to "manual" so students won't see the grades by default)
+        const assignId = JSON.parse(responseText).id;
+        $.ajax({
+            url: `/api/graphql`,
+            type: 'POST',
+            data: String.raw`{"operationName":"SetAssignmentPostPolicy","variables":{"assignmentId":"${assignId}","postManually":true},"query":"mutation SetAssignmentPostPolicy($assignmentId: ID!, $postManually: Boolean!) {\n  setAssignmentPostPolicy(input: {assignmentId: $assignmentId, postManually: $postManually}) {\n    postPolicy {\n      postManually\n      __typename\n    }\n    errors {\n      attribute\n      message\n      __typename\n    }\n    __typename\n  }\n}\n"}`,
+            contentType: "application/json",
+            dataType: "text"
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            popUp(`ERROR ${jqXHR.status} while muting assignment. Please delete the empty assignment before trying again.`, null);
+            $("#zoom_file").show();
+        }).done(function (responseText2) {
+
+            // prepare a list of requests to send
+            let requests = [];
+            for (const user of attendData.users) {
+                // build api url
+                const subUrl = `/api/v1/courses/${courseId}/assignments/${assignId}/submissions/${user.userId}`;
+                // build request and canned error message in case it fails
+                requests.push({
+                    request: {
+                        url: subUrl,
+                        type: "PUT",
+                        data: { 'submission[posted_grade]': user.minutes >= minMinutes ? assignPoints : 0 },
+                        dataType: "text" },
+                    error: `Failed to post score for student ${user.username} (id: ${user.userId}) using endpoint ${subUrl}. Response: `
+                });
             }
-        }
 
-        // prepare a list of requests to send
-        let requests = [];
-        for (const userGrade of userGrades) {
-            // build api url
-            const subUrl = `/api/v1/courses/${courseId}/assignments/${assignment.id}/submissions/${userGrade.user}`;
-            // build request and canned error message in case it fails
-            requests.push({
-                request: {
-                    url: subUrl,
-                    type: "PUT",
-                    data: { 'submission[posted_grade]': userGrade.grade },
-                    dataType: "text" },
-                error: `Failed to post score for student ${userGrade.username} (id: ${userGrade.user}) using endpoint ${subUrl}. Response: `
-            });
-        }
-
-        // send requests in chunks of 10 every second to avoid rate-limiting
-        var errors = [];
-        var completed = 0;
-        var chunkSize = 10;
-        function sendChunk(i) {
-            for (const request of requests.slice(i, i + chunkSize)) {
-                $.ajax(request.request).fail(function(jqXHR, textStatus, errorThrown) {
-                    if (jqXHR.status == 500) {
-                        // Canvas sometimes gets random server errors, so retry
-                        $.ajax(request.request).fail(function(jqXHR, textStatus, errorThrown) {
+            // send requests in chunks of 10 every second to avoid rate-limiting
+            var errors = [];
+            var completed = 0;
+            var chunkSize = 10;
+            function sendChunk(i) {
+                for (const request of requests.slice(i, i + chunkSize)) {
+                    $.ajax(request.request).fail(function(jqXHR, textStatus, errorThrown) {
+                        if (jqXHR.status == 500) {
+                            // Canvas sometimes gets random server errors, so retry
+                            $.ajax(request.request).fail(function(jqXHR, textStatus, errorThrown) {
+                                errors.push(`${request.error}${jqXHR.status} - ${errorThrown}\n`);
+                            });
+                        } else {
                             errors.push(`${request.error}${jqXHR.status} - ${errorThrown}\n`);
-                        });
-                    } else {
-                        errors.push(`${request.error}${jqXHR.status} - ${errorThrown}\n`);
-                    }
-                }).always(requestSent);
-            }
-            showProgress(i * 100 / requests.length);
-            if (i + chunkSize < requests.length) {
-                setTimeout(sendChunk, 1000, i + chunkSize);
-            }
-        }
-
-        // when each request finishes...
-        function requestSent() {
-            completed++;
-            if (completed >= requests.length) {
-                // all finished
-                showProgress(100);
-                $("#zoom_file").show();
-                if (errors.length > 0) {
-                    popUp(`Import complete. WARNING: ${errors.length} scores failed to import. See errors.txt for details and then manually enter the correct scores for these users.`, function() { window.location.href = window.location.href; });
-                    saveText(errors, "errors.txt");
-                } else {
-                    popUp("All scores imported successfully!", function() { window.location.href = window.location.href; });
+                        }
+                    }).always(requestSent);
+                }
+                showProgress(i * 100 / requests.length);
+                if (i + chunkSize < requests.length) {
+                    setTimeout(sendChunk, 1000, i + chunkSize);
                 }
             }
-        }
-        // actually starts the recursion
-        sendChunk(0);
+
+            // when each request finishes...
+            function requestSent() {
+                completed++;
+                if (completed >= requests.length) {
+                    // all finished
+                    showProgress(100);
+                    $("#zoom_file").show();
+                    if (errors.length > 0) {
+                        popUp(`Import complete. WARNING: ${errors.length} scores failed to import. See errors.txt for details and then manually enter the correct scores for these users.`, function() { window.location.href = window.location.href; });
+                        saveText(errors, "errors.txt");
+                    } else {
+                        popUp("All scores imported successfully!", function() { window.location.href = window.location.href; });
+                    }
+                }
+            }
+            // actually starts the recursion
+            sendChunk(0);
+        });
+
     });
+
 }
 
 defer(function() {
@@ -405,10 +360,6 @@ defer(function() {
     $("head").append($('<style type="text/css"> .no-close .ui-dialog-titlebar-close { display: none; } </style>'))
     $("body").append($('<div id="zoom_dialog" title="Import Zoom Attendance"></div>'));
     $("#zoom_dialog").dialog({ autoOpen: false, dialogClass: "no-close", closeOnEscape: false });
-
-    // prep jquery confirm dialog
-    $("body").append($('<div id="zoom_confirm" title="Import Zoom Attendance"></div>'));
-    $("#zoom_confirm").dialog({ autoOpen: false, dialogClass: "no-close", closeOnEscape: false });
 
     // prep jquery progress dialog
     $("body").append($('<div id="zoom_progress" title="Import Zoom Attendance"><p>Importing attendance. Do not navigate from this page.</p><div id="zoom_bar"></div></div>'));
@@ -430,9 +381,7 @@ defer(function() {
                 complete: function(results) {
                     parseImport(results.data, function(attendanceData) {
                         openOptionsForm(attendanceData.date, function(formData) {
-                            identifyAssignment(formData, function(assignment, submissions) {
-                                saveAttendance(formData, attendanceData, assignment, submissions);
-                            });
+                            saveAttendance(formData, attendanceData);
                         });
                     });}
             });
